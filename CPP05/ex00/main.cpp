5aadd8e834b65a9c012b457e61eb3709e108093f
spablob3 <joapedro@student.42porto.com>
2026-07-30 21:11:20 +0100

    ex00 main improved

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -3,21 +3,59 @@
 int main (void)
 {
 
-	/* //Copy constructor
-	Bureaucrat a ("joao", 4);
-	Bureaucrat b (a);
+	std::cout << "====High and Low grades====\n";
+	try{
+		
+		Bureaucrat don ("Don Cornelius", 151);
+		std::cout << don << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Caught an exception: " << e.what();
+	}
+	
+	try{
+		
+		Bureaucrat shelly("Shelly Ann Fraser", 50);
+		std::cout << shelly << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Caught an exception: " << e.what();
+	}
 
-	std::cout << "A: "<< a << std::endl;
-	std::cout << "B: "<< b << std::endl;
+	try{
+		
+		Bureaucrat bolt("Usain bolt", 0);
+		std::cout << bolt << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Caught an exception: " << e.what();
+	}
 
-	//Assignment operator
-	Bureaucrat c ("Ana", 150);
-	Bureaucrat d ("Manel", 50);
-
-	d = c;
-
-	std::cout << "C: "<< c << std::endl;
-	std::cout << "D: "<< d << std::endl; */
+	std::cout << "\n====Copy Constructor/Assignment====" << std::endl;
+	try
+	{
+		Bureaucrat a ("joao", 4);
+		Bureaucrat b (a);
+	
+		std::cout << "Original: " << a << std::endl;
+		std::cout << "Copy: " << b << std::endl;
+	
+		//Assignment operator
+		Bureaucrat c ("Ana", 150);
+		Bureaucrat d ("Manel", 50);
+	
+		d = c;
+	
+		std::cout << "Original: " << c << std::endl;
+		std::cout << "Assignment: " << d << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Caught an exception: " << e.what();
+	}
 
 	std::cout << "\n====Increment====" << std::endl;
 	try
@@ -50,50 +88,5 @@ int main (void)
 	{
 		std::cout << "Caught an exception: " << e.what();
 	}
-/* 	try{
-		Bureaucrat a ("A", 13);
-		Bureaucrat copy("Copy", 0);
-	
-		copy = a;
-	
-		std::cout << a << std::endl;
-		std::cout << copy << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		std::cout << "Caught an exception: " << e.what();
-	} */
-
-	/* try{
-		
-		Bureaucrat don ("Don Cornelius", 151);
-		std::cout << don << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		std::cout << "Caught an exception: " << e.what();
-	}
-	
-	try{
-		
-		Bureaucrat shelly("Shelly Ann Fraser", 50);
-		std::cout << shelly << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		std::cout << "Caught an exception: " << e.what();
-	}
-
-	try{
-		
-		Bureaucrat bolt("Usain bolt", 0);
-		std::cout << bolt << std::endl;
-	}
-	catch (std::exception & e)
-	{
-		std::cout << "Caught an exception: " << e.what();
-	}*/
-
-
 	return (0);
 }
