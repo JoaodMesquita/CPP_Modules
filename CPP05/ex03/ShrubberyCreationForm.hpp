d521bf6c211b2a9a9af22def123cbcc167b99c35
spablob3 <joapedro@student.42porto.com>
2026-08-01 13:19:20 +0100

    Added default constructors to all classes

--- CPP05/ex03/ShrubberyCreationForm.hpp
+++ CPP05/ex03/ShrubberyCreationForm.hpp
@@ -8,6 +8,7 @@ class ShrubberyCreationForm : public AForm{
 
 	public:
 
+		ShrubberyCreationForm();
 		ShrubberyCreationForm(std::string target);
 		ShrubberyCreationForm(const ShrubberyCreationForm& other);
 		ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
