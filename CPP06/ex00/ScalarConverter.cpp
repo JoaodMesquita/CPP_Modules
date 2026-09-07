ae960f784851f67ebd2e250f2af12c3397c5e29b
spablob3 <joapedro@student.42porto.com>
2026-09-07 22:08:49 +0100

    validated double literal

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -15,12 +15,12 @@ ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
 
 ScalarConverter::~ScalarConverter(){}
 
-int ScalarConverter::isChar(const std::string &input)
+int	ScalarConverter::isChar(const std::string &input)
 {
 	return (input.length() == 1 && !isdigit(input[0]));
 }
 
-int ScalarConverter::isInt(const std::string &input)
+int	ScalarConverter::isInt(const std::string &input)
 {
 	int	i = 0;
 	if (input[0] == '-' || input[0] == '+')
@@ -63,7 +63,33 @@ int	ScalarConverter::isFloat(const std::string &input)
 	return (0);
 }
 
-literal ScalarConverter::identifyLiteral(const std::string &input)
+int	ScalarConverter::isDouble(const std::string &input)
+{
+	size_t i = 0;
+	if (input[0] == '-' || input[0] == '+')
+		i++;
+	size_t pos = input.find('.');
+	if (pos == std::string::npos)
+		return (0);
+	while (input[i])
+	{
+		if (!isdigit(input[i]) && i < pos)
+			return (0);
+		i++;
+	}
+	++pos;
+	if (input[pos] == '\0')
+		return (0);
+	while (input[pos])
+	{
+		if (!isdigit(input[pos]))
+			return (0);
+		pos++;
+	}
+	return (1);
+}
+
+literal	ScalarConverter::identifyLiteral(const std::string &input)
 {
 	if (isChar(input))
 		return CHAR;
@@ -71,8 +97,8 @@ literal ScalarConverter::identifyLiteral(const std::string &input)
 		return INT;
 	else if (isFloat(input))
 		return FLOAT;
-	/* else if (isDouble(input))
-		return DOUBLE; */
+	else if (isDouble(input))
+		return DOUBLE;
 	else
 		return INVALID;
 }
@@ -82,3 +108,5 @@ void ScalarConverter::convert(std::string &input)
 	literal type = identifyLiteral(input);
 	std::cout << type << "\n";
 }
+
+
