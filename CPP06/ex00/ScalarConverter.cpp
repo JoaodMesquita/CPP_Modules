5cba5a559231fff5a34b1f61c5e5e1a3d522f078
Joao Pedro De Mesquita Fernandes <jpmesquita29085@hotmail.com>
2026-09-08 16:24:43 +0100

    need to fix conversion to float because is always adding .0 or 0.f to every number

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -135,7 +135,27 @@ void convertFloat(const std::string &input)
 		std::string str = input;
 		str.erase(str.length() - 1);
 		std::cout << "double: " << str << "\n";
+		return;
 	}
+
+	long num;
+	std::stringstream ss(input);
+	ss >> num;
+
+	if (num < 0 || num > 127)
+		std::cout << "char: impossible" << "\n";
+	else if (num >= 0 && num <= 31)
+		std::cout << "char: Non displayable" << '\n';
+	else
+		std::cout << "char: " << static_cast<char>(num) << "\n";
+	
+	if (num >= std::numeric_limits<int>::min() && num <= std::numeric_limits<int>::max())
+		std::cout << "int: " << num << "\n";
+	else
+		std::cout << "int: impossible" << "\n";
+
+	std::cout << "float: " << static_cast<float>(num) << ".0f" << "\n";
+	std::cout << "double: " << static_cast<double>(num) << ".0" << "\n";
 }
 
 literal	ScalarConverter::identifyLiteral(const std::string &input)
