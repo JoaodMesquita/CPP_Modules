54f1e8ec685783296d70925c8007b60a2a4ed9d7
Joao Pedro De Mesquita Fernandes <jpmesquita29085@hotmail.com>
2026-09-21 13:01:31 +0100

    final tests done

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -2,11 +2,15 @@
 #define ITER_HPP
 
 #include <iostream>
+#include <cstring>
 
 template <typename T, typename F> void iter (T *array, const int length, F function)
 {
-	if (!array)
+	if (!array || !length)
+	{
+		std::cout << "Error: Empty!\n";
 		return ;
+	}
 	for (int i = 0; i < length; i++)
 		function(array[i]);
 }
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,8 +1,61 @@
 #include "iter.hpp"
 
+void	printInt(const int &number)
+{
+	std::cout << number << "\n";
+}
+
+void	printChar(const char &c)
+{
+	std::cout << c << "\n";
+}
+
+void	printDouble(const double &d)
+{
+	std::cout << d << "\n";
+}
+
+void doubleValue(double &number)
+{
+	number *= 2;
+}
+
+void increment(int &number)
+{
+	number++;
+}
+
 int	main(void)
 {
 
+	int array[5] = {1, 2 ,3 ,4 ,5};
+	char charArray[4] = {'a', 'b', 'c', 'd'};
+	double doubleArray[3] = {1.5, 4.75, 10.69};
+	const int constArray[5] = {5,4,3,2,1};
+
+	std::cout << "=====Print Int=====\n";
+	::iter(array, 5, printInt);
+
+	std::cout << "=====Increment Int=====\n";
+	::iter(array, 5, increment);
+	::iter(array, 5, printInt);
+
+	std::cout << "=====Print Char=====\n";
+	::iter(charArray, 4, printChar);
+
+	std::cout << "=====Print Double=====\n";
+	::iter(doubleArray, 3, printDouble);
+
+	std::cout << "=====Double value (double)=====\n";
+	::iter(doubleArray, 3, doubleValue);
+	::iter(doubleArray, 3, printDouble);
+
+	std::cout << "=====Empty array=====\n";
+	::iter(array, 0, printInt);
+
+	std::cout << "=====const array=====\n";
+	::iter(constArray, 5, printInt);
+	::iter(array, 5, increment); // cant modify values because const var.
 	
 	return 0;
 }
